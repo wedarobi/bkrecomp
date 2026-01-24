6e81827a05dd3cdbb51ef9a91923131da418fe25
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2026-01-24 13:35:01 -0500

    1.0.0

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -64,7 +64,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "1.0.0-rc2";
+const std::string version_string = "1.0.0";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
